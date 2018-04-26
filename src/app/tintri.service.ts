6aa304e520b6ebbe47005b4eba54a90d258ef017
aayush050 <aayushsingla08701282gmail.com>
2018-04-26 16:40:43 +0530

    data from server

--- src/app/tintri.service.ts
+++ src/app/tintri.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {Tintri} from './tintri'
+import { Tintri } from './tintri'
 import { Observable } from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class TintriService {
 
-private _url: string = "/assets/data/tintri.json";
+  public data:any;
+  private _url: string = 'http://192.168.106.141:8083';
+  private parseData(res: Response) {
+    return res.json() || [];
+  }
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
+  //public getData(): Observable<Tintri[]> {
+    //eturn this.http
+     // .get(this._url + '/user')
+     // .catch(this.errorHandler);
+  //}
 
-getData():Observable<Tintri[]>{
-return this.http.get<Tintri[]>(this._url)
-                  .catch(this.errorHandler)
-}
-errorHandler(error:HttpErrorResponse){
-  return Observable.throw(error.message || "Server Error")
-}
+  getConfig(): Observable<Tintri[]> { 
+    this.data=this.http.get('http://192.168.106.141:8083/user');
+    console.log("data",this.data)
+    return this.data.catch(this.errorHandler);
+  }
+
+
+  private extractData(res: Response) {
+    return res.json() || [];
+  }
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throw(error.message || "Server Error")
+  }
+
 }
--- src/app/web/web.component.ts
+++ src/app/web/web.component.ts
@@ -7,21 +7,21 @@ HttpClient
   template: `
   <html>
   <h1>{{errorMessage}}</h1>
-  <table height="200" width="20%" border-style="solid" *ngFor="let data of tintri">
+  <table border="0" height="200" width="20%"  *ngFor="let data of tintri">
    <tr >
     <td colspan=2>
-    <div class="status"><img width="20" src="{{data.src}}"> <b> {{data.status}} </b> </div>
+    <div class="status"><img width="15" src="{{data.src}}"> <b> {{data.status}} </b> <img align ="right"  width = "30" src="http://2.bp.blogspot.com/_t2t0TocmM7M/STBhJF6P4-I/AAAAAAAAE3A/dnG7EPdL3Rw/s400/centerellipsis_l.gif"></div>
       <div class="name"><b>{{data.name}}</b></div>
       <div class="description"><b>{{data.description}}</b></div>
       <div class="path"><b>{{data.path}}</b></div>
     </td>
     </tr>
     <tr >
-      <td>
+      <td class = "vmborder">
         <div class="vm"><b>VMs</b></div> 
         <div class="vmcount"><b>{{data.number}}</b></div>
       </td>
-       <td>
+       <td class="errorborder">
         <div class="error"><b>ERROR</b></div>
         <div class ="errorcount"><b>{{data.error}}</b></div>
         </td>
@@ -33,14 +33,15 @@ HttpClient
   styles: []
 })
 export class WebComponent implements OnInit {
-public tintri = [];
+public tintri:any=[];
 public errorMessage;
   constructor(private Tintri:TintriService) { }
 
   ngOnInit() {
-    this.Tintri.getData()
+    this.Tintri.getConfig()
     .subscribe(data =>this.tintri = data,
                     error => this.errorMessage = error);
+    //this.tintri = this.Tintri.getConfig();
   }
 
 }
